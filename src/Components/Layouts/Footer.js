ffd5a03576dcec0b3dd14bf9147897ba9e1347a5
Emanoel <emanoelromano@gmail.com>
2018-06-01 15:54:05 -0300

    a

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Paper, Tabs, Tab, withWidth } from '@material-ui/core'
+import {Paper, Tabs, Tab, withWidth} from '@material-ui/core'
 
 export default withWidth()(
-  ({ muscles, category, onSelect, width }) => {
-  
-    const index = category 
-    ? muscles.findIndex(group => group === category) +1
-    : 0
+    ({muscles, category, onSelect, width}) => {
 
-    const onIndexSelect = (e, index) =>
-      onSelect(index === 0 ? '' : muscles[index - 1])
+        const index = category
+            ? muscles.findIndex(group => group === category) + 1
+            : 0;
 
-    return <Paper>
-      <Tabs
-        value={index}
-        onChange={onIndexSelect}
-        indicatorColor="primary"
-        textColor="primary"
-        centered={width !== 'xs'}
-        scrollable={width === 'xs'}
-      >
-        <Tab label="ALL" />
-        {muscles.map(group =>
-          <Tab key={group} label={group} />
-        )}
-      </Tabs>
-    </Paper>
-  })
+        const onIndexSelect = (e, index) =>
+            onSelect(index === 0 ? '' : muscles[index - 1]);
+
+        return <Paper>
+            <Tabs
+                value={index}
+                onChange={onIndexSelect}
+                indicatorColor="primary"
+                textColor="primary"
+                centered={width !== 'xs'}
+                scrollable={width === 'xs'}
+            >
+                <Tab label="ALL"/>
+                {muscles.map(group =>
+                    <Tab key={group} label={group}/>
+                )}
+            </Tabs>
+        </Paper>
+    })
